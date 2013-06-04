a77664d6019c11e17f5a90f816bbc0f41ee95539
Lasath Fernando <kde@lasath.org>
2013-06-04 22:06:31 +1000

    Make reload.sh restart kwin

--- contents/code/reload.sh
+++ contents/code/reload.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-qdbus org.kde.KWin /Scripting org.kde.kwin.Scripting.unloadScript "kmonad"
+#qdbus org.kde.KWin /Scripting org.kde.kwin.Scripting.unloadScript "kmonad"
+
+kwin --replace &
 
 qdbus org.kde.KWin /Scripting org.kde.kwin.Scripting.loadScript "$(pwd)/main.js" "kmonad"
 
