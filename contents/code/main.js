2cddb37b947a12265e06dcc8db6a93900f7f6665
Lasath Fernando <kde@lasath.org>
2013-06-04 18:17:18 +1000

    Make it avoid unwanted windows
    I.e. special windows (like plasma-desktop) and windows on other
    desktops. It does so by keeping its own list of clients instead
    of using workspace.clientList() every time.

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,61 +1,64 @@
 
 
-var clients = workspace.clientList(); 
-print(clients[0].caption);
-
 print(workspace.clientArea(workspace.MaximizeArea,workspace.activeScreen,workspace.currentDesktop).width);
 print(workspace.clientArea(workspace.MaximizeArea,workspace.activeScreen,workspace.currentDesktop).height);
-var screenGeom = 
+var screenGeom =
 workspace.clientArea(workspace.MaximizeArea,workspace.activeScreen,workspace.currentDesktop);
 
-for (w in clients){
-  if (clients[w].windowRole == "panel_1" ){
-    print("removing: " + clients[w].caption)
-    clients.splice(w,1); 
-  }
+var potentialClients = workspace.clientList();
+var clients = new Array();
+
+for (w in potentialClients){
+    if (!potentialClients[w].specialWindow &&
+        potentialClients[w].desktop == workspace.currentDesktop) {
+        clients.push(potentialClients[w]);
+    } else {
+        print("Skipping special window '" + potentialClients[w].caption + "'");
+    }
 }
 
 
 function spiral(clients,geom){
-  if(clients.length > 0){
-    var wnd = clients.shift();
-    print(wnd.caption + ":" + wnd.windowRole);
-    if (wnd.windowRole != "panel_1" ){
-      if (geom.width > geom.height){
-	geom.width = (geom.width/2);
-	wnd.geometry = geom;
-	geom.x = geom.x+wnd.width;
-      }else{
-	geom.height = (geom.height/2);
-	wnd.geometry = geom;
-	geom.y = geom.y+wnd.height;
-      }
-    } 
-    spiral(clients,geom);
-  }
+    if(clients.length > 0){
+        var wnd = clients.shift();
+        print(wnd.caption + ":" + wnd.windowRole);
+        if (wnd.windowRole != "panel_1" ){
+            if (geom.width > geom.height){
+                geom.width = (geom.width/2);
+                wnd.geometry = geom;
+                geom.x = geom.x+wnd.width;
+            } else{
+                geom.height = (geom.height/2);
+                wnd.geometry = geom;
+                geom.y = geom.y+wnd.height;
+            }
+        }
+        spiral(clients,geom);
+    }
 }
 // spiral(clients, screenGeom);
 
- tallMode(clients, screenGeom);
+tallMode(clients, screenGeom);
 
 
 function tallMode(clients, geom) {
-  var main = clients.shift(); 
-  print(main.caption);
-  mainGeom = geom;
-  mainGeom.width = geom.width / 2;
-  main.geometry = mainGeom;
-  mainGeom.x += mainGeom.width;
-  stackVertically(clients, mainGeom);
+    var main = clients.shift();
+    print(main.caption);
+    mainGeom = geom;
+    mainGeom.width = geom.width / 2;
+    main.geometry = mainGeom;
+    mainGeom.x += mainGeom.width;
+    stackVertically(clients, mainGeom);
 }
 
 function stackVertically(clients, geom) {
-  var height = geom.height / clients.length;
+    var height = geom.height / clients.length;
+    var vOffset = geom.y;
 
-  for (w in clients) {
-    geom.y = height * w;
-    geom.height = height;
-    
-    clients[w].geometry = geom;
-  }
+    for (w in clients) {
+        geom.y = (height * w) + vOffset;
+        geom.height = height;
+
+        clients[w].geometry = geom;
+    }
 }
