6c5d0c3934518c35b3bc5a51191323cebb7fa7de
Elliott Lester <lester.elliott@gmail.com>
2013-06-12 23:32:30 +1000

    Fix shouldIgnore now skips minimized windows

--- contents/code/main.js
+++ contents/code/main.js
@@ -28,6 +28,11 @@ function ClientList() {
             return true;
         }
 
+        //to avoid trying to tile minimized windows
+        if (pc.minimized) {
+            return true;
+        }
+
         // to avoid things like Yakuake
         if (pc.skipPager || pc.skipTaskbar) {
             return true;
